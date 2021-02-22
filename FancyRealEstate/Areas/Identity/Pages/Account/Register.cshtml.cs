02d161eff2733099f7f0160e1335eada0463a45c
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-02-23 00:02:17 +0200

    Add Names And Phone To Register

--- FancyRealEstate/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ FancyRealEstate/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -47,6 +47,21 @@ namespace FancyRealEstate.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [StringLength(30, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+            [Display(Name="First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [StringLength(30, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+            [Display(Name ="Last Name")]
+            public string LastName { get; set; }
+            
+            [Required]
+            [Display(Name ="Phone Numbert")]
+            public string PhoneNumber { get; set; }
+
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -75,7 +90,7 @@ namespace FancyRealEstate.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, PhoneNumber = Input.PhoneNumber, FirstName = Input.FirstName, LastName = Input.LastName };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
--- FancyRealEstate/Models/ApplicationUser.cs
+++ FancyRealEstate/Models/ApplicationUser.cs
@@ -4,5 +4,8 @@ namespace FancyRealEstate.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
     }
 }
