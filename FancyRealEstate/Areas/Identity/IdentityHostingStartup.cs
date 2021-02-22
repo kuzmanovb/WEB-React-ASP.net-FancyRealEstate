af74634a2a276d6eb3d44e04994ab3665237617a
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-02-22 17:23:29 +0200

    Scaffolding Identity Register

---
+++ FancyRealEstate/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FancyRealEstate.Data;
+using FancyRealEstate.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FancyRealEstate.Areas.Identity.IdentityHostingStartup))]
+namespace FancyRealEstate.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
