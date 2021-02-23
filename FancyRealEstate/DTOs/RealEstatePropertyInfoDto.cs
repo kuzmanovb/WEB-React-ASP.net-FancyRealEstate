488514ce1c25826b3a1d1dfcc1ddb3f8bde5138d
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-02-24 01:59:03 +0200

    Update RealEstatePropertyInfoDto.cs

--- FancyRealEstate/DTOs/RealEstatePropertyInfoDto.cs
+++ FancyRealEstate/DTOs/RealEstatePropertyInfoDto.cs
@@ -19,6 +19,18 @@ namespace FancyRealEstate.DTOs
 
         public double Price { get; set; }
 
+        public string Street { get; set; }
+
+        public string District { get; set; }
+
+        public string City { get; set; }
+
+        public string BuildingNumber { get; set; }
+
+        public string PropertyType { get; set; }
+
+        public int BuildingType { get; set; }
+
         public string Description { get; set; }
 
         public bool IsPromotion { get; set; }
@@ -27,22 +39,26 @@ namespace FancyRealEstate.DTOs
 
         public DateTime CreatedOn { get; set; }
 
-        public DateTime? ModifiedOn { get; set; }
+        public string FullName { get; set; }
 
-        public bool IsDeleted { get; set; }
+        public string PhoneNumber { get; set; }
 
-        public DateTime? DeletedOn { get; set; }
-
-        public int SellerId { get; set; }
-
-        public int AddressId { get; set; }
-
-        public int FeatureId { get; set; }
-
-        public string PropertyType { get; set; }
-
-        public string BuildingType { get; set; }
+        public string Email { get; set; }
 
         public  ICollection<string> ImagesUrl { get; set; }
+
+        public bool Internet { get; set; }
+
+        public bool Heating { get; set; }
+
+        public bool SecuritySistem { get; set; }
+
+        public bool AirCondition { get; set; }
+
+        public bool Garage { get; set; }
+
+        public bool Elevator { get; set; }
+
+        public bool Renovated { get; set; }
     }
 }
