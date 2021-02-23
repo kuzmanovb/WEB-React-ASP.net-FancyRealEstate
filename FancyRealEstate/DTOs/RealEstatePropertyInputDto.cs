1f100226dc247abc8968b042224b50fd0865c8e6
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-02-24 01:47:03 +0200

    Add Is Promotion To Real Estate Property

---
+++ FancyRealEstate/DTOs/RealEstatePropertyInputDto.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace FancyRealEstate.DTOs
+{
+    public class RealEstatePropertyInputDto
+    {
+        public int Size { get; set; }
+
+        public int? Floor { get; set; }
+
+        public int? TotalNumberOfFloor { get; set; }
+
+        public int Year { get; set; }
+
+        public double Price { get; set; }
+
+        public string Street { get; set; }
+
+        public string District { get; set; }
+
+        public string City { get; set; }
+
+        public string BuildingNumber { get; set; }
+
+        public string PropertyType { get; set; }
+
+        public int BuildingType { get; set; }
+
+        public string Description { get; set; }
+
+        public string TypeOfDeal { get; set; }
+
+        public bool IsPromotion { get; set; }
+
+        public string UserId { get; set; }
+
+        public virtual ICollection<string> ImagesUrl { get; set; }
+
+        public bool Internet { get; set; }
+
+        public bool Heating { get; set; }
+
+        public bool SecuritySistem { get; set; }
+
+        public bool AirCondition { get; set; }
+
+        public bool Garage { get; set; }
+
+        public bool Elevator { get; set; }
+
+        public bool Renovated { get; set; }
+
+    }
+}
---
+++ FancyRealEstate/Data/Migrations/20210223234450_AddIsPromotionToRealEstateProperty.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FancyRealEstate.Data.Migrations
+{
+    public partial class AddIsPromotionToRealEstateProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RealEstateProperties_Cities_CityId",
+                table: "RealEstateProperties");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RealEstateProperties_CityId",
+                table: "RealEstateProperties");
+
+            migrationBuilder.DropColumn(
+                name: "CityId",
+                table: "RealEstateProperties");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPromotion",
+                table: "RealEstateProperties",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPromotion",
+                table: "RealEstateProperties");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CityId",
+                table: "RealEstateProperties",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RealEstateProperties_CityId",
+                table: "RealEstateProperties",
+                column: "CityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RealEstateProperties_Cities_CityId",
+                table: "RealEstateProperties",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
