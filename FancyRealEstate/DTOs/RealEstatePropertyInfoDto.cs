ccb496edc32e955e306918007b68640fe0e9fabc
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-02-25 17:55:45 +0200

    Update RealEstatePropertyInfoDto.cs

--- FancyRealEstate/DTOs/RealEstatePropertyInfoDto.cs
+++ FancyRealEstate/DTOs/RealEstatePropertyInfoDto.cs
@@ -43,7 +43,7 @@
 
         public string Email { get; set; }
 
-        public  ICollection<string> ImagesUrl { get; set; }
+        public ICollection<string> ImagesUrl { get; set; }
 
         public bool Internet { get; set; }
 
