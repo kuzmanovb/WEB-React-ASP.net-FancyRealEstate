ec5d49158a2a0f108295f4abe9fc0b3d497ec94d
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-02-26 11:42:10 +0200

    Create PropertyTypesService

---
+++ FancyRealEstate/Services/Contracts/IProperyTypesService.cs
@@ -0,0 +1,17 @@
+﻿namespace FancyRealEstate.Services.Contracts
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using FancyRealEstate.Models;
+
+    public interface IProperyTypesService
+    {
+        Task<int> CreatePropertyTypeAsync(string name);
+
+        PropertyType GetPropertyTypeByName(string name);
+
+        ICollection<string> GetAllPropertyTypeName();
+
+        Task DeletePropertyTypeAsync(string name);
+    }
+}
---
+++ FancyRealEstate/Services/ProperyTypesService.cs
@@ -0,0 +1,56 @@
+﻿namespace FancyRealEstate.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using FancyRealEstate.Data;
+    using FancyRealEstate.Models;
+    using FancyRealEstate.Services.Contracts;
+
+    public class ProperyTypesService : IProperyTypesService
+    {
+        private readonly ApplicationDbContext db;
+
+        public ProperyTypesService(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<int> CreatePropertyTypeAsync(string name)
+        {
+            var newPropertyType = new PropertyType { Name = name };
+
+            await this.db.PropertyTypes.AddAsync(newPropertyType);
+            await this.db.SaveChangesAsync();
+
+            return newPropertyType.Id;
+        }
+
+        public ICollection<string> GetAllPropertyTypeName()
+        {
+            var allPropertyType = this.db.PropertyTypes.OrderBy(x => x.Name).Select(p => p.Name).ToArray();
+
+            return allPropertyType;
+        }
+
+        public PropertyType GetPropertyTypeByName(string name)
+        {
+            var currentPropertyType = this.db.PropertyTypes.Where(p => p.Name == name).FirstOrDefault();
+
+            return currentPropertyType;
+        }
+
+        public async Task DeletePropertyTypeAsync(string name)
+        {
+            var currentPropertyType = this.db.PropertyTypes.Where(p => p.Name == name).FirstOrDefault();
+
+            if (currentPropertyType != null)
+            {
+                this.db.PropertyTypes.Remove(currentPropertyType);
+                await this.db.SaveChangesAsync();
+            }
+        }
+    }
+}
