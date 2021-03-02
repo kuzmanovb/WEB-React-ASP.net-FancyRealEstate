148b75df711a1e881573d96137225b37557c4dcb
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-03-02 14:30:43 +0200

    Create RealEstatePropertiesController.cs

---
+++ FancyRealEstate/Controllers/RealEstatePropertiesController.cs
@@ -0,0 +1,78 @@
+﻿namespace FancyRealEstate.Controllers
+{
+    using System.Threading.Tasks;
+    using FancyRealEstate.DTOs;
+    using FancyRealEstate.Services.Contracts;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class RealEstatePropertiesController : ControllerBase
+    {
+        private readonly ILogger<RealEstatePropertiesController> logger;
+        private readonly IRealEstatePropertiesService realEstatePropertiesService;
+
+        public RealEstatePropertiesController(ILogger<RealEstatePropertiesController> logger, IRealEstatePropertiesService realEstatePropertiesService)
+        {
+            this.logger = logger;
+            this.realEstatePropertiesService = realEstatePropertiesService;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+
+            var currentProperty = this.realEstatePropertiesService.GetPropertiesWithPredicate(x => x.Id == id);
+
+            if (currentProperty == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(currentProperty);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] RealEstatePropertyInputDto input)
+        {
+            var newRealEstatePropertyId = await this.realEstatePropertiesService.CreateRealEstateProperyAsync(input);
+
+            if (newRealEstatePropertyId != 0)
+            {
+                return this.StatusCode(201);
+            }
+
+            return this.BadRequest();
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] RealEstatePropertyInfoDto input)
+        {
+            var currentProperty = this.realEstatePropertiesService.GetPropertiesWithPredicate(x => x.Id == input.Id);
+
+            if (currentProperty == null)
+            {
+                return this.NotFound();
+            }
+
+            await this.realEstatePropertiesService.UpdatePropertyAsync(input);
+
+            return this.Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await this.realEstatePropertiesService.DeletePropertyAsync(id);
+
+            if (result)
+            {
+                return this.Ok();
+            }
+
+            return this.BadRequest();
+        }
+    }
+}
