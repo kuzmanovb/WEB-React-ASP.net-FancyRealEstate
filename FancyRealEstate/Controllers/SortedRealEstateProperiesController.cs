a33dadfc02ce9f27c44afbc7838652a56c5593d5
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-03-20 11:09:39 +0200

    Create SortedRealEstateProperiesController

---
+++ FancyRealEstate/Controllers/SortedRealEstateProperiesController.cs
@@ -0,0 +1,41 @@
+﻿using FancyRealEstate.DTOs;
+using FancyRealEstate.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FancyRealEstate.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SortedRealEstateProperiesController : ControllerBase
+    {
+        private const int NumberPropertyToPage = 9;
+
+        private readonly ILogger<SortedRealEstateProperiesController> logger;
+        private readonly IRealEstatePropertiesService realEstatePropertiesService;
+
+        public SortedRealEstateProperiesController(ILogger<SortedRealEstateProperiesController> logger, IRealEstatePropertiesService realEstatePropertiesService)
+        {
+            this.logger = logger;
+            this.realEstatePropertiesService = realEstatePropertiesService;
+        }
+
+        [HttpPost]
+        public IActionResult GetSorted([FromBody] SortedRealestatePropertiesDto input)
+        {
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest();
+            }
+
+            var properties = this.realEstatePropertiesService.GetSortedProperties(input);
+
+            return this.Ok(properties);
+        }
+    }
+}
