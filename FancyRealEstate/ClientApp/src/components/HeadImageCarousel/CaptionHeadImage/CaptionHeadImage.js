af966c6a9c5a57f8e47743d1809e4f0d4a6f10fb
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-03-22 12:51:24 +0200

    Updare button style

--- FancyRealEstate/ClientApp/src/components/HeadImageCarousel/CaptionHeadImage/CaptionHeadImage.js
+++ FancyRealEstate/ClientApp/src/components/HeadImageCarousel/CaptionHeadImage/CaptionHeadImage.js
@@ -10,7 +10,7 @@ export const CaptionHeadImage = (prop) => {
                     <span className="d-inline-block bg-danger text-white px-3 mb-3 property-offer-type rounded">For Sale</span>
                     <h1 className="mb-2">625 S. Berendo St</h1>
                     <p className="mb-5"><strong className="h2 text-light font-weight-bold">$1,000,500</strong></p>
-                    <p><a href="#" className="btn btn-white btn-outline-white py-3 px-5 rounded-0 btn-2">See Details</a></p>
+                    <p><a href="#" className="btn btn-outline-light py-3 px-5 rounded-0 btn-2">See Details</a></p>
                 </div>
             </div>
         </div>
