d1e1b3075cc56f1b20784b12c5290a2865a76e31
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-03-23 13:40:51 +0200

    Replace A Tag With link

--- FancyRealEstate/ClientApp/src/components/Gallery/ImageFrame/ImageFrame.js
+++ FancyRealEstate/ClientApp/src/components/Gallery/ImageFrame/ImageFrame.js
@@ -1,4 +1,6 @@
-﻿import React from 'react'
+﻿/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from 'react'
+import {Link} from 'react-router-dom'
 import './ImageFrame.css'
 
 export const ImageFrame = (props) => {
@@ -6,15 +8,15 @@ export const ImageFrame = (props) => {
 
         <div class="col-md-6 col-lg-4 mb-4">
             <div class="property-entry h-100">
-                <a href="property-details.html" class="property-thumbnail">
+                <Link to="/property-details" class="property-thumbnail">
                     <div class="offer-type-wrap">
                         <span class="offer-type bg-danger active">Sale</span>
                         <span class="offer-type bg-success">Rent</span>
                     </div>
                     <img src="hero_bg_1.jpg" alt="Image" class="img-fluid" />
-                </a>
+                </Link>
                 <div class="p-4 property-body">
-                    <h2 class="property-title"><a href="property-details.html">625 S. Berendo St</a></h2>
+                    <h2 class="property-title"><Link to="/property-details">625 S. Berendo St</Link></h2>
                     <span class="property-location d-block mb-3"><span class="property-icon icon-room"></span> 625 S. Berendo St Unit 607 Los Angeles, CA 90005</span>
                     <strong class="property-price text-primary mb-3 d-block text-success">$2,265,500</strong>
                     <ul class="property-specs-wrap mb-3 mb-lg-0">
