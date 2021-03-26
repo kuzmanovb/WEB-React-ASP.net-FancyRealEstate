60786f938b05c10f2212c8eb8e4086d1e6e9c74e
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-03-26 23:39:54 +0200

    Create Property Details

--- FancyRealEstate/ClientApp/src/components/PropertyDetails.js
+++ FancyRealEstate/ClientApp/src/components/PropertyDetails.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { HeadImageCarousel } from './HeadImageCarousel/HeadImageCarousel'
 import { PropertyDetailsInfo } from './PropertyDetailsInfo/PropertyDetailsInfo'
+import { RelatedProperties } from './RelatedProperties/RelatedProperties'
 import { Footer } from './Footer/Footer'
 
 export const PropertyDetails = () => {
@@ -8,6 +9,7 @@ export const PropertyDetails = () => {
         <div>
             <HeadImageCarousel />
             <PropertyDetailsInfo />
+            <RelatedProperties />
             <Footer />
         </div>
 
