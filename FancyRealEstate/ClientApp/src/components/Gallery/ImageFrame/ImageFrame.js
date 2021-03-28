003063da3548c402b0ae505ba48fea91d2ba272f
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-03-29 01:28:11 +0300

    Add Cloudinary Component For Image

--- FancyRealEstate/ClientApp/src/components/Gallery/ImageFrame/ImageFrame.js
+++ FancyRealEstate/ClientApp/src/components/Gallery/ImageFrame/ImageFrame.js
@@ -1,20 +1,23 @@
 ﻿/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './ImageFrame.css'
+import { ImageFromCloudinary } from '../../Cloudinary/ImageFromCloudinary'
 
 export const ImageFrame = (props) => {
     return (
 
         <div className="col-md-6 col-lg-4 mb-4">
             <div className="property-entry h-100">
+                
                 <Link to="/property-details" className="property-thumbnail">
                     <div className="offer-type-wrap">
                         <span className="offer-type bg-danger active">Sale</span>
                         <span className="offer-type bg-success">Rent</span>
                     </div>
-                    <img src="hero_bg_1.jpg" alt="Image" className="img-fluid" />
+                    <ImageFromCloudinary publicId="FancyRealEstate-FirstReactApp/bh94v7uutzzcnprpx9xl" />
                 </Link>
+
                 <div className="p-4 property-body">
                     <h2 className="property-title"><Link to="/property-details">625 S. Berendo St</Link></h2>
                     <span className="property-location d-block mb-3"><span className="property-icon icon-room"></span> 625 S. Berendo St Unit 607 Los Angeles, CA 90005</span>
@@ -36,7 +39,6 @@ export const ImageFrame = (props) => {
 
                         </li>
                     </ul>
-
                 </div>
             </div>
         </div>
