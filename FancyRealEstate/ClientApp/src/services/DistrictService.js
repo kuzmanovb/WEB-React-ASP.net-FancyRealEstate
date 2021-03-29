380d732b9cc3f7f805c8d9778f71826fa3092173
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-03-30 01:40:40 +0300

    Create District Service

---
+++ FancyRealEstate/ClientApp/src/services/DistrictService.js
@@ -0,0 +1,28 @@
+import { url } from './UrlService'
+
+const destrictUrl = url + "districts";
+
+export const getAll = () => {
+
+    return fetch(destrictUrl)
+        .then(res => res.json())
+        .catch(error => console.log(error));
+};
+
+export const getByName = (name) => {
+    return fetch(destrictUrl + `/${name}`)
+    .then(res => res.json())
+    .catch(error => console.log(error));
+ };
+
+export const create = (name) => {
+    return fetch(destrictUrl + `/${name}`,{
+        method: "PUT",
+    });
+ };
+
+export const deleted = (name) => { 
+    return fetch(destrictUrl + `/${name}`,{
+        method: "DELETE"
+    });
+};
