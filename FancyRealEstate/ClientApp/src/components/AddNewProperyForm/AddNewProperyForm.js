f92e206882c16a7ae1b6d986c47227dc45ac73ab
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-04-05 01:34:50 +0300

    Add View For Uploade Image

--- FancyRealEstate/ClientApp/src/components/AddNewProperyForm/AddNewProperyForm.js
+++ FancyRealEstate/ClientApp/src/components/AddNewProperyForm/AddNewProperyForm.js
@@ -4,29 +4,31 @@ import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { Formik } from 'formik'
 import * as Yup from 'yup'
 import './AddNewProperyForm.css'
+import { ImageFromCloudinary } from '../Cloudinary/ImageFromCloudinary'
 import { AddressRow } from './FormComponents/AddressRow'
 import { PropertyTypeForm } from './FormComponents/PropertyTypeForm'
 import { CloudinaryWidget } from './FormComponents/CloudinaryWidget'
 import { BuildingTypeForm } from './FormComponents/BuildingTypeForm'
 import { FeaturesForm } from './FormComponents/FeaturesForm'
+import * as propertyService from '../../services/propertyService'
 
-const validationSchema =  Yup.object().shape({
+const validationSchema = Yup.object().shape({
 
-    city: Yup.string().min(2).required( "You have choose city"),
-    district: Yup.string().min(2).required( "You have choose district"),
-    street: Yup.string().min(4).required( "Please imput street name"),
-    buildingNumber: Yup.string().min(1).required( "Please imput street number"),
-    propertyType: Yup.string().min(2).required( "You have choose property type"),
+    city: Yup.string().min(2).required("You have choose city"),
+    district: Yup.string().min(2).required("You have choose district"),
+    street: Yup.string().min(4).required("Please imput street name"),
+    buildingNumber: Yup.string().min(1).required("Please imput street number"),
+    propertyType: Yup.string().min(2).required("You have choose property type"),
     year: Yup.string().min(4).required("Please input year between 1900 and present year"),
     size: Yup.string().min(2).required("Please input size"),
-    floor: Yup.string().min(1).required( "Please input floor"),
-    totalNumberOfFloor: Yup.string().min(1).required( "Please input of total floors"),
-    typeOfDeal: Yup.string().min(2).required( "You have choose type of deal"),
-    price: Yup.string().min(2).required( "Please input price"),
-    buildingType: Yup.string().min(2).required( "You have choose building type"),
+    floor: Yup.string().min(1).required("Please input floor"),
+    totalNumberOfFloor: Yup.string().min(1).required("Please input of total floors"),
+    typeOfDeal: Yup.string().min(2).required("You have choose type of deal"),
+    price: Yup.string().min(2).required("Please input price"),
+    buildingType: Yup.string().min(2).required("You have choose building type"),
     description: Yup.string().min(10, "Description must have at least 10 characters")
-                             .max(1000, "Description can't be longer than 1000 characters")
-                             .required("Please write short information about the property"),
+        .max(1000, "Description can't be longer than 1000 characters")
+        .required("Please write short information about the property"),
     imageIds: Yup.array().min(1, "You must have not less of one photo"),
 
 });
@@ -69,22 +71,31 @@ export class AddNewProperyForm extends Component {
         this.setState({ buildingType: buildingType })
     }
 
-    featuresDataFromChild = (features) =>{
-        this.setState({features: features})
+    featuresDataFromChild = (features) => {
+        this.setState({ features: features })
     }
-  
+
     imagesIdDateFromChild = (imageId) => {
         this.setState((s) => ({ imageIds: [...s.imageIds, ...imageId] }));
     }
 
-    handleStateValue = (e) =>{
+    handleStateValue = (e) => {
 
         let propName = e.target.name;
         let propValue = e.target.value;
 
-        this.setState({[propName]: propValue});
+        this.setState({ [propName]: propValue });
 
     };
+
+    redirect = () => {
+        this.props.history.push("/dashboard");
+    }
+
+    deleteImage = (e) => {
+        console.log(e.target.value)
+
+    }
 
     render() {
 
@@ -96,14 +107,15 @@ export class AddNewProperyForm extends Component {
                     initialValues={this.state}
                     validationSchema={validationSchema}
                     onSubmit={(values, { setSubmitting, resetForm }) => {
-                        // When button submits form and form is in the process of submitting, submit button is disabled
                         setSubmitting(true);
 
-                        // Simulate submitting to database, shows us values submitted, resets form
+                        propertyService.createProperty(values, null, 2);
+
+
                         setTimeout(() => {
-                            alert(JSON.stringify(values, null, 2));
-                            resetForm();
-                            setSubmitting(false);
+
+                            this.redirect()
+
                         }, 500);
                     }}
 
@@ -116,7 +128,7 @@ export class AddNewProperyForm extends Component {
                         handleSubmit,
                         isSubmitting }) => (
                         <Form className="mt-5" onSubmit={handleSubmit}>
-                            <AddressRow addressData={this.addressDataFronChild} passBlur={handleBlur} passTouched={touched} passErrors={errors}/>
+                            <AddressRow addressData={this.addressDataFronChild} passBlur={handleBlur} passTouched={touched} passErrors={errors} />
                             <Row form>
                                 <Col md={4}>
                                     <PropertyTypeForm propertyTypeData={this.propertyTypeDateFromChild} passTouched={touched} passErrors={errors} />
@@ -124,29 +136,29 @@ export class AddNewProperyForm extends Component {
                                 <Col md={2}>
                                     <FormGroup>
                                         <Label for="year">Year</Label>
-                                        <Input type="number" name="year" id="year" min={1900} max={2021} value={values.year} onChange={this.handleStateValue} onBlur={handleBlur}/>
-                                        {touched.year && errors.year ? <div className="text-danger">{errors.year}</div>: null}
+                                        <Input type="number" name="year" id="year" min={1900} max={2021} value={values.year} onChange={this.handleStateValue} onBlur={handleBlur} />
+                                        {touched.year && errors.year ? <div className="text-danger">{errors.year}</div> : null}
                                     </FormGroup>
                                 </Col>
                                 <Col md={2}>
                                     <FormGroup>
                                         <Label for="size">Size</Label>
-                                        <Input type="number" name="size" id="size" min={0} value={values.size} onChange={this.handleStateValue} onBlur={handleBlur}/>
-                                        {touched.size && errors.size ? <div className="text-danger">{errors.size}</div>: null}
+                                        <Input type="number" name="size" id="size" min={0} value={values.size} onChange={this.handleStateValue} onBlur={handleBlur} />
+                                        {touched.size && errors.size ? <div className="text-danger">{errors.size}</div> : null}
                                     </FormGroup>
                                 </Col>
                                 <Col md={2}>
                                     <FormGroup>
                                         <Label for="floor">Floor</Label>
-                                        <Input type="number" name="floor" id="floor" min={0} value={values.floor} onChange={this.handleStateValue} onBlur={handleBlur}/>
-                                        {touched.floor && errors.floor ? <div className="text-danger">{errors.floor}</div>: null}
+                                        <Input type="number" name="floor" id="floor" min={0} value={values.floor} onChange={this.handleStateValue} onBlur={handleBlur} />
+                                        {touched.floor && errors.floor ? <div className="text-danger">{errors.floor}</div> : null}
                                     </FormGroup>
                                 </Col>
                                 <Col md={2}>
                                     <FormGroup>
                                         <Label for="totalNumberOfFloor">Of Total Floors</Label>
-                                        <Input type="number" name="totalNumberOfFloor" id="totalNumberOfFloor" min={0} value={values.totalNumberOfFloor} onChange={this.handleStateValue} onBlur={handleBlur}/>
-                                        {touched.totalNumberOfFloor && errors.totalNumberOfFloor ? <div className="text-danger">{errors.totalNumberOfFloor}</div>: null}
+                                        <Input type="number" name="totalNumberOfFloor" id="totalNumberOfFloor" min={0} value={values.totalNumberOfFloor} onChange={this.handleStateValue} onBlur={handleBlur} />
+                                        {touched.totalNumberOfFloor && errors.totalNumberOfFloor ? <div className="text-danger">{errors.totalNumberOfFloor}</div> : null}
                                     </FormGroup>
                                 </Col>
                             </Row>
@@ -157,36 +169,47 @@ export class AddNewProperyForm extends Component {
                                         <Label for="typeOfDeal">Type Of Deal</Label>
                                         <Input type="select" name="typeOfDeal" id="typeOfDeal" value={values.typeOfDeal} onChange={this.handleStateValue} onBlur={handleBlur}>
                                             <option value="">Choose Deal</option>
-                                            <option value="forRent">Rent</option>
-                                            <option value="forDeal">Deal</option>
+                                            <option value="Rent">Rent</option>
+                                            <option value="Sale">Sale</option>
                                         </Input>
-                                        {touched.typeOfDeal && errors.typeOfDeal ? <div className="text-danger">{errors.typeOfDeal}</div>: null}
+                                        {touched.typeOfDeal && errors.typeOfDeal ? <div className="text-danger">{errors.typeOfDeal}</div> : null}
                                     </FormGroup>
-                                    
+
                                 </Col>
                                 <Col md={4}>
-                                <FormGroup>
+                                    <FormGroup>
                                         <Label for="price">Price</Label>
-                                        <Input type="number" name="price" id="price" min={0} value={values.price} onChange={this.handleStateValue} onBlur={handleBlur}/>
-                                        {touched.price && errors.price ? <div className="text-danger">{errors.price}</div>: null}
+                                        <Input type="number" name="price" id="price" min={0} value={values.price} onChange={this.handleStateValue} onBlur={handleBlur} />
+                                        {touched.price && errors.price ? <div className="text-danger">{errors.price}</div> : null}
                                     </FormGroup>
                                 </Col>
                             </Row>
                             <Row form className="row justify-content-around">
                                 <Col md={2}>
-                                    <BuildingTypeForm buildingTypeData={this.buildingTypeDataFromChild} passTouched={touched} passErrors={errors}/>
+                                    <BuildingTypeForm buildingTypeData={this.buildingTypeDataFromChild} passTouched={touched} passErrors={errors} />
                                 </Col>
                                 <Col md={4}>
-                                  <FeaturesForm featuresData={this.featuresDataFromChild} passTouched={touched} passErrors={errors}/>
+                                    <FeaturesForm featuresData={this.featuresDataFromChild} passTouched={touched} passErrors={errors} />
                                 </Col>
                             </Row>
-                            <CloudinaryWidget imagesIdData={this.imagesIdDateFromChild} passTouched={touched} passErrors={errors}/>
+                            <Row>
+                                {this.state.imageIds.map((i) => (
+                                    <div key={i} className="col-sm-6 col-md-4 col-lg-3">
+                                        <ImageFromCloudinary publicId={i} />
+                                        <Button color="danger" value={i} onClick={this.deleteImage}>Delete Image</Button>
+                                    </div>
+                                ))}
+                            </Row>
+                            <Row>
+                                <div></div>
+                            </Row>
+                            <CloudinaryWidget imagesIdData={this.imagesIdDateFromChild} passTouched={touched} passErrors={errors} />
                             <Row>
                                 <Col>
                                     <FormGroup>
                                         <Label for="description">Description</Label>
-                                        <Input type="textarea" name="description" id="description" rows="10" onChange={this.handleStateValue} onBlur={handleBlur}/>
-                                        {touched.description && errors.description ? <div className="text-danger">{errors.description}</div>: null}
+                                        <Input type="textarea" name="description" id="description" rows="10" onChange={this.handleStateValue} onBlur={handleBlur} />
+                                        {touched.description && errors.description ? <div className="text-danger">{errors.description}</div> : null}
                                     </FormGroup>
                                 </Col>
                             </Row>
