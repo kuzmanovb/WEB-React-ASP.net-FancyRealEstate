7e72b6fe715221c453bb17c9b491256b519c376c
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-04-05 01:31:16 +0300

    Refactoring For Use Fetch Data

--- FancyRealEstate/ClientApp/src/components/Dashboard/Dashboard.js
+++ FancyRealEstate/ClientApp/src/components/Dashboard/Dashboard.js
@@ -1,61 +1,64 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
-import {Button} from 'reactstrap'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
+import { Button } from 'reactstrap'
 import './Dashboard.css';
-import {DashboardRow} from './DashboardRow/DashboardRow'
+import { DashboardRow } from './DashboardRow/DashboardRow'
+import * as propertyService from '../../services/propertyService'
 
 
-export class Dashboard extends Component {
-    constructor(props) {
-        super(props);
-    }
+export const Dashboard = (props) => {
 
-    
-    render() {
-        return (
-            <div className="mt-5">
-                <div className="container">
+    const [realEstateProperties, setrealEstateProperties] = useState([]);
 
-                    <div className="row">
-                        <div className="col">
-                            <div className="my-property font-weight-bold">My Properties</div>
-                            <p className="show-properties">Showing 5 Results</p>
-                        </div>
-                        <div className="col-6">
-                        </div>
-                        <div className="col">
-                        <Link to="add-property"><Button outline color="primary" size="lg">Add Property</Button>{' '}</Link>
-                        </div>
+    useEffect(() => {
+
+        propertyService.getByUserId({ "userId": "e104c908-7e48-474c-8acf-cdd5abe92eac" }).then(res => setrealEstateProperties(res))
+
+    }, [])
+
+    useEffect(() => {
+
+        console.log(realEstateProperties)
+    }, [realEstateProperties])
+
+    return (
+        <div className="mt-5">
+            <div className="container">
+
+                <div className="row">
+                    <div className="col">
+                        <div className="my-property font-weight-bold">My Properties</div>
+                        <p className="show-properties">Showing {realEstateProperties.length} Results</p>
                     </div>
-
-                    <div className="row bg-light pt-3 pb-3">
-                        <div className="col-6 text-center">
-                            PROPERTY
-                        </div>
-                        <div className="col-3 text-center">
-                            POSTED ON
-                        </div>
-                        <div className="col-3 text-center">
-                            ACTIONS
-                         </div>
+                    <div className="col-6">
                     </div>
-
-                    
-
-                    <DashboardRow city="Sofia" address="jk.Ilinden bl.52" postOn="12.06.2020" price="120 000 lv."/>
-                    <DashboardRow city="Sofia" address="jk.Ilinden bl.52" postOn="12.06.2020" price="120 000 lv."/>
-                    <DashboardRow city="Sofia" address="jk.Ilinden bl.52" postOn="12.06.2020" price="120 000 lv."/>
-                    <DashboardRow city="Sofia" address="jk.Ilinden bl.52" postOn="12.06.2020" price="120 000 lv."/>
-                    <DashboardRow city="Sofia" address="jk.Ilinden bl.52" postOn="12.06.2020" price="120 000 lv."/>
-                    
-
-
+                    <div className="col">
+                        <Link to="add-property"><Button outline color="primary" size="lg">Add Property</Button></Link>
+                    </div>
                 </div>
+
+                <div className="row bg-light pt-3 pb-3">
+                    <div className="col-6 text-center">
+                        PROPERTY
+                        </div>
+                    <div className="col-3 text-center">
+                        POSTED ON
+                        </div>
+                    <div className="col-3 text-center">
+                        ACTIONS
+                         </div>
+                </div>
+
+                {realEstateProperties.map((res) =>
+
+                    <DashboardRow key={res.id} data={res} imageId={res.imageIds[0]} />
+                )}
+
             </div>
+        </div>
 
 
-        );
-    }
+    );
 }
 
 
--- FancyRealEstate/ClientApp/src/components/Dashboard/DashboardRow/DashboardRow.js
+++ FancyRealEstate/ClientApp/src/components/Dashboard/DashboardRow/DashboardRow.js
@@ -1,46 +1,50 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Media } from 'reactstrap';
 import './DashboardRow.css'
+import {cloudinaryUrl} from '../../../services/cloudinaryUrl'
 
 
-export class DashboardRow extends Component {
-  constructor(props) {
-    super(props);
-  }
+export const DashboardRow = (props) => {
+  const [data, setData] = useState([])
 
+  useEffect(() => {
 
-  render() {
-    return (
+    setData(...Object.values(props))
+    console.log(data)
 
-      <>
-        <div className="row">
-          <div className="col-6">
-            <Media className="mt-2">
-              <Media object src="hero_bg_1.jpg" alt="Generic placeholder image" className="dashboard-image" />
-              <Media body className="pl-3">
-                <Media heading className="pt-2">
-                  <b>{this.props.city}</b>
-                  <h5><i>{this.props.address}</i></h5>
-                </Media>
-                <p>{this.props.price}</p>
+  }, [data])
+
+  const imageUrl = cloudinaryUrl() + props.imageId
+
+  return (
+    <>
+      <div className="row">
+        <div className="col-6">
+          <Media className="mt-2">
+            <Media object src= {imageUrl} alt="Generic placeholder image" className="dashboard-image" />
+            <Media body className="pl-3">
+              <Media heading className="pt-2">
+                <b>{data.city}</b>
+                <h5><i>jk.{data.district} bl.{data.buildingNumber}</i></h5>
               </Media>
+              <p>{data.price} lv.</p>
             </Media>
-          </div>
-          <div className="col-3 text-center mt-4">
-            <h5>{this.props.postOn}</h5>
-            <h5><i>Days ago</i></h5>
+          </Media>
+        </div>
+        <div className="col-3 text-center mt-4">
+          <h5>{data.createdOn}</h5>
+          <h5><i>{data.dateAgo} Days ago</i></h5>
 
-          </div>
-          <div className="col-3 text-center">
-            <div class="btn-group-vertical">
-              <button type="button" className="btn btn-success mt-3">Edit</button>
-              <button type="button" className="btn btn-danger mt-2">Delete</button>
+        </div>
+        <div className="col-3 text-center">
+          <div className="btn-group-vertical">
+            <button type="button" className="btn btn-success mt-3">Edit</button>
+            <button type="button" className="btn btn-danger mt-2">Delete</button>
 
-            </div>
           </div>
         </div>
-        <div style={{ border: '2px solid Gainsboro' }} />
-      </>
-    );
-  }
+      </div>
+      <div style={{ border: '2px solid Gainsboro' }} />
+    </>
+  );
 }
