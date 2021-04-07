c1c315ab3d1c5ea9895518fe77b52189d43d954d
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-04-08 02:13:39 +0300

    Fix Bug With Fetch

--- FancyRealEstate/ClientApp/src/components/Home.js
+++ FancyRealEstate/ClientApp/src/components/Home.js
@@ -1,26 +1,30 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import { HeadImageCarousel } from './HeadImageCarousel/HeadImageCarousel.js'
 import { Search } from './Search/Search.js'
 import { Sort } from './Sort/Sort'
 import { Gallery } from './Gallery/Gallery.js'
 import { Footer } from './Footer/Footer.js'
+import * as propertyService from '../services/propertyService'
 
 
 export const Home = () => {
     // cost displayName = Home.name;
-    const [page, setPage] = useState("")
+    const [properties, setProperties] = useState([])
+    const [page, setPage] = useState("0")
     const [city, setCity] = useState("")
     const [district, setDistrict] = useState("")
     const [propertyType, setPropertyType] = useState("")
     const [deal, setDeal] = useState("")
     const [price, setPrice] = useState("")
     const [date, setDate] = useState("")
-    const [minPrice, setMinPrice] = useState("");
-    const [maxPrice, setMaxPrice] = useState("");
+    const [minPrice, setMinPrice] = useState("0");
+    const [maxPrice, setMaxPrice] = useState("0");
 
 
     useEffect(() => {
-       
+
+        propertyService.getAll(parametersForRequest).then(res => setProperties(res))
 
     }, [city, district, propertyType, deal, date, price, minPrice, maxPrice])
 
@@ -38,15 +42,17 @@ export const Home = () => {
         setMaxPrice(maxPriceValue)
     }
 
-    const parametersForRequest = {"page": page, "city": city, "district": district, "propertyType": propertyType,
-                                  "priceByPrice": price, "priceByDate": date ,"deal": deal, "minPrice": minPrice, "maxPrice" : maxPrice }
+    const parametersForRequest = {
+        "page": page, "city": city, "district": district, "propertyType": propertyType,
+        "sortByPrice": price, "sortByDate": date, "deal": deal, "minPrice": minPrice, "maxPrice": maxPrice,
+    }
 
     return (
         <div>
             <HeadImageCarousel />
             <Search getData={dataFromSearch} />
             <Sort getData={dataFromSort} />
-            <Gallery />
+            <Gallery allProperties={properties}/>
             <Footer />
 
         </div>
