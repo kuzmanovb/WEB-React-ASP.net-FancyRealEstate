0033aee639b703c2d86fe655396366de1a169608
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-04-08 02:14:06 +0300

    Add Props

--- FancyRealEstate/ClientApp/src/components/PropertyDetails.js
+++ FancyRealEstate/ClientApp/src/components/PropertyDetails.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import { HeadImageCarousel } from './HeadImageCarousel/HeadImageCarousel'
 import { PropertyDetailsInfo } from './PropertyDetailsInfo/PropertyDetailsInfo'
 import { RelatedProperties } from './RelatedProperties/RelatedProperties'
 import { Footer } from './Footer/Footer'
 
-export const PropertyDetails = () => {
+export const PropertyDetails = (props) => {
+
+useEffect(() => {
+ console.log(props)
+}, [props.location?.state !== undefined])
+
     return (
         <div>
             <HeadImageCarousel />
-            <PropertyDetailsInfo />
+            <PropertyDetailsInfo data={props}/>
             <RelatedProperties />
             <Footer />
         </div>
