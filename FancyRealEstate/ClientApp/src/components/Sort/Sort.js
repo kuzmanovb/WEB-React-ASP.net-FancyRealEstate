06f4e661394a7461c7c215876d69140384a9e069
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-04-07 13:38:46 +0300

    Fix Bug With passDate Function

--- FancyRealEstate/ClientApp/src/components/Sort/Sort.js
+++ FancyRealEstate/ClientApp/src/components/Sort/Sort.js
@@ -4,46 +4,66 @@ import './Sort.css'
 export const Sort = (props) => {
 
     const [deal, setDeal] = useState("")
-    const [sort, setSort] = useState("")
+    const [sortPrice, setSortPrice] = useState("")
     const [date, setDate] = useState("")
+    const [minPrice, setMinPrice] = useState("")
+    const [maxPrice, setMaxPrice] = useState("")
 
     useEffect(() => {
 
-        props.getData(deal,sort, date)
+        passData();
 
-    }, [deal, sort, date])
+    }, [deal, sortPrice, date, minPrice, maxPrice])
 
     const getDeal = (e) => {
         setDeal(e.target.value)
     };
 
     const getPrice = (e) => {
-        setSort(e.target.value)
+        setSortPrice(e.target.value)
     };
 
-    
     const getDate = (e) => {
         setDate(e.target.value)
     };
+
+    const getMinPrice = (e) => {
+        setMinPrice(e.target.value)
+    };
+
+    const getMaxPrice = (e) => {
+        setMaxPrice(e.target.value)
+    };
+
+    const passData = () => {
+        props.getData(deal, sortPrice, date, minPrice, maxPrice)
+    };
+
 
     return (
         <div className="container">
             <div className="row">
                 <div className="py-3 px-3 d-md-flex align-items-center">
                     <div>
-                        <button className="btn btn-outline-success px-3" value="" onClick={getDeal} >All</button>
-                        <button className="btn btn-outline-success px-3" value="rent" onClick={getDeal}>Rent</button>
-                        <button className="btn btn-outline-success px-3" value="sele" onClick={getDeal}>Sale</button>
+                        <button className="btn btn-outline-success px-3" name="deal" value="" onClick={getDeal} >All</button>
+                        <button className="btn btn-outline-success px-3" name="deal" value="rent" onClick={getDeal}>Rent</button>
+                        <button className="btn btn-outline-success px-3" name="deal" value="sele" onClick={getDeal}>Sale</button>
+                    </div>
+                    <div className="select-wrap col-2">
+                        <input type="number" className="form-control" name="minPrice" placeholder="Min Price" onClick={getMinPrice} onBlur={getMinPrice} />
+                    </div>
+                    <div className="select-wrap col-2">
+                        <input type="number" className="form-control" name="maxPrice" placeholder="Max Price" onClick={getMaxPrice} onBlur={getMaxPrice} />
                     </div>
                     <div className="select-wrap ml-3">
-                        <select className="form-control-sort form-control-sort-sm d-block rounded-0" onClick={getPrice}>
+                        <select className="form-control-sort form-control-sort-sm d-block rounded-0" name="price" onClick={getPrice}>
                             <option value="">Sort by Price</option>
                             <option value="ascending">Ascending</option>
                             <option value="descending">Descending</option>
                         </select>
                     </div>
                     <div className="select-wrap ml-3">
-                        <select className="form-control-sort form-control-sort-sm d-block rounded-0" onClick={getDate}>
+                        <select className="form-control-sort form-control-sort-sm d-block rounded-0" name="date" onClick={getDate}>
                             <option value="">Sort by Date</option>
                             <option value="ascending">Ascending</option>
                             <option value="descending">Descending</option>
