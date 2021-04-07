eafa52d3f0bacb8c26fa946a243eeebd82b6b569
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-04-08 02:06:42 +0300

    Update For Work With Props

--- FancyRealEstate/ClientApp/src/components/Gallery/Gallery.js
+++ FancyRealEstate/ClientApp/src/components/Gallery/Gallery.js
@@ -1,23 +1,22 @@
-﻿import React from 'react'
+﻿import React, { useEffect } from 'react'
 import { ImageFrame } from './ImageFrame/ImageFrame.js'
 import { Pagination } from '../Pagination/Pagination.js'
 
 export const Gallery = (props) => {
+
     return (
         <div className="bg-light pt-4 pb-4 mb-5">
             <div className="container">
                 <div className="row mb-5">
-                    <ImageFrame />
-                    <ImageFrame />
-                    <ImageFrame />
-                    <ImageFrame />
-                    <ImageFrame />
+                    {props.allProperties.map((p) =>
+
+                        <ImageFrame key={p.id} data={p} />
+
+                    )}
                 </div>
             </div>
             <Pagination />
         </div>
-
-
 
     );
 }
