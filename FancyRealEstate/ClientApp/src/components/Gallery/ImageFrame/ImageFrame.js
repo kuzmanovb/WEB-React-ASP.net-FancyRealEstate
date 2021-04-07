1a73f458af45445d323f856654e46521546a3afa
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-04-08 02:07:34 +0300

    Update For Work With Props

--- FancyRealEstate/ClientApp/src/components/Gallery/ImageFrame/ImageFrame.js
+++ FancyRealEstate/ClientApp/src/components/Gallery/ImageFrame/ImageFrame.js
@@ -1,41 +1,41 @@
 ﻿/* eslint-disable jsx-a11y/img-redundant-alt */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Button } from 'reactstrap';
 import { Link } from 'react-router-dom'
 import './ImageFrame.css'
 import { ImageFromCloudinary } from '../../Cloudinary/ImageFromCloudinary'
 
 export const ImageFrame = (props) => {
+
     return (
 
         <div className="col-md-6 col-lg-4 mb-4">
             <div className="property-entry h-100">
-                
-                <Link to="/property-details" className="property-thumbnail">
+
+                <Link to={{ pathname: `/property-details/${props.data?.id}`, state: props }} className="property-thumbnail">
                     <div className="offer-type-wrap">
-                        <span className="offer-type bg-danger active">Sale</span>
-                        <span className="offer-type bg-success">Rent</span>
+                        {props.data?.typeOfDeal === "Sale" && <span className="offer-type bg-danger active">Sale</span>}
+                        {props.data?.typeOfDeal === "Rent" && <span className="offer-type bg-success">Rent</span>}
                     </div>
-                    <ImageFromCloudinary publicId="FancyRealEstate-FirstReactApp/bh94v7uutzzcnprpx9xl" />
+                    <ImageFromCloudinary publicId={props.data?.imageIds[0]} />
                 </Link>
 
                 <div className="p-4 property-body">
-                    <h2 className="property-title"><Link to="/property-details">625 S. Berendo St</Link></h2>
-                    <span className="property-location d-block mb-3"><span className="property-icon icon-room"></span> 625 S. Berendo St Unit 607 Los Angeles, CA 90005</span>
-                    <strong className="property-price text-primary mb-3 d-block text-success">$2,265,500</strong>
+                    <Button outline color="success" className="property-title text-center">
+                        <Link to={{pathname: `/property-details/${props.data?.id}`, state:props}}><b>View details</b></Link>
+                    </Button>
+                    <span className="property-location d-block mb-3"><h5><i>city</i>: &nbsp;{props.data?.city}<br /><i>destrict</i>:&nbsp; {props.data?.district}</h5></span>
+                    <strong className="property-price text-primary mb-3 d-block text-success">price:&nbsp; {props.data?.price} &euro;</strong>
                     <ul className="property-specs-wrap mb-3 mb-lg-0">
                         <li>
-                            <span className="property-specs">Beds</span>
-                            <span className="property-specs-number">2 <sup>+</sup></span>
+                            <span className="property-specs">size</span>
+                            <span className="property-specs-number">{props.data?.size} m<sup>2</sup></span>
 
                         </li>
+                        <li>{props.data?.id}</li>
                         <li>
-                            <span className="property-specs">Baths</span>
-                            <span className="property-specs-number">2</span>
-
-                        </li>
-                        <li>
-                            <span className="property-specs">SQ FT</span>
-                            <span className="property-specs-number">7,000</span>
+                            <span className="property-specs">type</span>
+                            <span className="property-specs-number">{props.data?.propertyType}</span>
 
                         </li>
                     </ul>
