08cab4b83920b6ddf026f31d9ae6238bf1d84200
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-04-07 11:33:36 +0300

    Update And Pass Values To Home

--- FancyRealEstate/ClientApp/src/components/Sort/Sort.js
+++ FancyRealEstate/ClientApp/src/components/Sort/Sort.js
@@ -1,26 +1,57 @@
-﻿import React from 'react'
+﻿import React, { useState, useEffect } from 'react'
 import './Sort.css'
 
 export const Sort = (props) => {
+
+    const [deal, setDeal] = useState("")
+    const [sort, setSort] = useState("")
+    const [date, setDate] = useState("")
+
+    useEffect(() => {
+
+        props.getData(deal,sort, date)
+
+    }, [deal, sort, date])
+
+    const getDeal = (e) => {
+        setDeal(e.target.value)
+    };
+
+    const getPrice = (e) => {
+        setSort(e.target.value)
+    };
+
+    
+    const getDate = (e) => {
+        setDate(e.target.value)
+    };
+
     return (
         <div className="container">
             <div className="row">
-                    <div className="py-3 px-3 d-md-flex align-items-center">
-                            <div>
-                            <a href="#" className="btn btn-outline-success px-3">All</a>
-                            <a href="#" className="btn btn-outline-success px-3">Rent</a>
-                            <a href="#" className="btn btn-outline-success px-3">Sale</a>
-                            </div>
-                        <div className="select-wrap ml-3">
-                                <select className="form-control-sort form-control-sort-sm d-block rounded-0">
-                                    <option value="">Sort by</option>
-                                    <option value="">Price Ascending</option>
-                                    <option value="">Price Descending</option>
-                                </select>
-                            </div>
+                <div className="py-3 px-3 d-md-flex align-items-center">
+                    <div>
+                        <button className="btn btn-outline-success px-3" value="" onClick={getDeal} >All</button>
+                        <button className="btn btn-outline-success px-3" value="rent" onClick={getDeal}>Rent</button>
+                        <button className="btn btn-outline-success px-3" value="sele" onClick={getDeal}>Sale</button>
+                    </div>
+                    <div className="select-wrap ml-3">
+                        <select className="form-control-sort form-control-sort-sm d-block rounded-0" onClick={getPrice}>
+                            <option value="">Sort by Price</option>
+                            <option value="ascending">Ascending</option>
+                            <option value="descending">Descending</option>
+                        </select>
+                    </div>
+                    <div className="select-wrap ml-3">
+                        <select className="form-control-sort form-control-sort-sm d-block rounded-0" onClick={getDate}>
+                            <option value="">Sort by Date</option>
+                            <option value="ascending">Ascending</option>
+                            <option value="descending">Descending</option>
+                        </select>
                     </div>
                 </div>
             </div>
+        </div>
 
 
     );
