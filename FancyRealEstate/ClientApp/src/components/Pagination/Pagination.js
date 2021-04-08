a230c8382a79a7be4995bf2c69f8c1cb5dd116ef
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-04-08 17:19:00 +0300

    Create Pagination

--- FancyRealEstate/ClientApp/src/components/Pagination/Pagination.js
+++ FancyRealEstate/ClientApp/src/components/Pagination/Pagination.js
@@ -1,19 +1,48 @@
-﻿import React from 'react'
-import {Link} from 'react-router-dom'
+﻿import React, { useState, useEffect } from 'react'
 import './Pagination.css'
 
-export const Pagination = () => {
+export const Pagination = (props) => {
+
+    const [pages, setPages] = useState(1)
+    const [currentPage, setCurrentPage] = useState(1)
+
+    useEffect(() => {
+
+        setPages(Math.ceil(props.data.length / 9))
+
+    }, [props])
+
+    const handleStateValue = (e) => {
+
+        props.getNumber(e.target.value)
+        setCurrentPage(e.target.value)
+    };
+
+    const numberPages = () => {
+
+        let numbers = [];
+
+        for (let i = 1; i <= pages; i++) {
+
+            numbers.push(
+
+                <button type="button" className={currentPage == i ? "active" : ""} key={i} value={i} onClick={handleStateValue}>
+                    {i}
+                </button>
+            )
+        }
+
+        return numbers
+    }
+
+
     return (
         <div className="row">
             <div className="col-md-12 text-center">
                 <div className="site-pagination">
-                    <Link to="#" className="active">1</Link>
-                    <Link to="#">2</Link>
-                    <Link to="#">3</Link>
-                    <Link to="#">4</Link>
-                    <Link to="#">5</Link>
-                    <span>...</span>
-                    <Link to="#">10</Link>
+                    {numberPages().map((i) => (
+                        i
+                    ))}
                 </div>
             </div>
         </div>);
