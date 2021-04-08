e7afb8c207d52d1e8a4fd1d49499fd334ece1a00
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2021-04-08 17:48:56 +0300

    Add Building Type

--- FancyRealEstate/ClientApp/src/components/Gallery/ImageFrame/ImageFrame.js
+++ FancyRealEstate/ClientApp/src/components/Gallery/ImageFrame/ImageFrame.js
@@ -27,15 +27,19 @@ export const ImageFrame = (props) => {
                     <span className="property-location d-block mb-3"><h5><i>city</i>: &nbsp;{props.data?.city}<br /><i>destrict</i>:&nbsp; {props.data?.district}</h5></span>
                     <strong className="property-price text-primary mb-3 d-block text-success">price:&nbsp; {props.data?.price} &euro;</strong>
                     <ul className="property-specs-wrap mb-3 mb-lg-0">
-                        <li>
+                        <li className="pr-2 text-center">
                             <span className="property-specs">size</span>
                             <span className="property-specs-number">{props.data?.size} m<sup>2</sup></span>
 
                         </li>
-                        <li>{props.data?.id}</li>
-                        <li>
-                            <span className="property-specs">type</span>
+                        <li className="pr-2 text-center">
+                            <span className="property-specs">property type</span>
                             <span className="property-specs-number">{props.data?.propertyType}</span>
+
+                        </li>
+                        <li className="pr-2 text-center">
+                            <span className="property-specs">building type</span>
+                            <span className="property-specs-number">{props.data?.buildingType}</span>
 
                         </li>
                     </ul>
