86855d333a3d06da7601a5b9f38073065d36b87f
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2022-07-11 12:34:27 +0300

    Create Country Service

---
+++ FancyRealEstate/Services/Contracts/ICountriesService.cs
@@ -0,0 +1,18 @@
+﻿namespace FancyRealEstate.Services.Contracts
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using FancyRealEstate.Models;
+
+    public interface ICountriesService
+    {
+
+        Task<int> CreateCountryAsync(string name);
+
+        Country GetCountryByName(string name);
+
+        ICollection<string> GetAllCountriesName();
+
+        Task<bool> DeleteCountryAsync(string name);
+    }
+}
---
+++ FancyRealEstate/Services/CountriesService.cs
@@ -0,0 +1,58 @@
+﻿namespace FancyRealEstate.Services
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using FancyRealEstate.Data;
+    using FancyRealEstate.Models;
+    using FancyRealEstate.Services.Contracts;
+
+    public class CountriesService : ICountriesService
+    {
+        private ApplicationDbContext db;
+
+        public CountriesService(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<int> CreateCountryAsync(string name)
+        {
+            var newCountry = new Country { Name = name };
+
+            await this.db.AddAsync(newCountry);
+            await this.db.SaveChangesAsync();
+
+            return newCountry.Id;
+        }
+
+        public ICollection<string> GetAllCountriesName()
+        {
+            var allCountries = this.db.Countries.OrderBy(x => x.Name).Select(x => x.Name).ToArray();
+
+            return allCountries;
+        }
+
+        public Country GetCountryByName(string name)
+        {
+           var country = this.db.Countries.FirstOrDefault(x => x.Name == name);
+
+           return country;
+        }
+
+        public async Task<bool> DeleteCountryAsync(string name)
+        {
+            var country = this.db.Countries.FirstOrDefault(x => x.Name == name);
+
+            if (country != null)
+            {
+                this.db.Countries.Remove(country);
+                await this.db.SaveChangesAsync();
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
