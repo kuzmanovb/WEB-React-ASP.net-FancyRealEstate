62239dcb044506c0f6705fecbe66f0d00fc3809a
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2022-09-06 15:50:35 +0300

    Fix bug, missing country and city to address

---
+++ FancyRealEstate/Data/Migrations/20220905123752_CityAndCountryToAddress.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FancyRealEstate.Data.Migrations
+{
+    public partial class CityAndCountryToAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CityId",
+                table: "Addresses",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CountryId",
+                table: "Addresses",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_CityId",
+                table: "Addresses",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_CountryId",
+                table: "Addresses",
+                column: "CountryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Cities_CityId",
+                table: "Addresses",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Countries_CountryId",
+                table: "Addresses",
+                column: "CountryId",
+                principalTable: "Countries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Cities_CityId",
+                table: "Addresses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Countries_CountryId",
+                table: "Addresses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Addresses_CityId",
+                table: "Addresses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Addresses_CountryId",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "CityId",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "CountryId",
+                table: "Addresses");
+        }
+    }
+}
